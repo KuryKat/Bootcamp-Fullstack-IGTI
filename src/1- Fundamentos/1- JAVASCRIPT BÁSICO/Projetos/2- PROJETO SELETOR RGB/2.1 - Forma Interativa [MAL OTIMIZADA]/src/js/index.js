109a7c9be37ec370f3bda078afe232e3a7808e6c
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-10-01 12:26:49 -0300

    send files from the first fudamentals matter: "Basic JavaScript"
    Files:
    
    - Exercise Folder
    - Projects Folder

---
+++ src/1- Fundamentos/1- JAVASCRIPT BÁSICO/Projetos/2- PROJETO SELETOR RGB/2.1 - Forma Interativa [MAL OTIMIZADA]/src/js/index.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', start)
+
+function start() {
+    setInterval(() => {
+        defineColor()
+    }, 50);
+}
+
+
+function defineColor() {
+    let body = document.querySelector("body")
+    let redUI = document.querySelector("#redUI")
+    let greenUI = document.querySelector("#greenUI")
+    let blueUI = document.querySelector("#blueUI")
+
+
+    let R = document.querySelector("#redRange")
+    let G = document.querySelector("#greenRange")
+    let B = document.querySelector("#blueRange")
+
+    body.style.backgroundColor = `rgb(${R.value},${G.value},${B.value})`;
+    redUI.value = R.value
+    greenUI.value = G.value
+    blueUI.value = B.value
+}
