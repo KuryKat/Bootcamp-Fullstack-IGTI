b264abc0bc9b2deea6cefe3609e3acb8e0ce6680
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-10-20 05:32:51 -0300

    Create CORS Exercise

---
+++ src/2- Backend/Exercícios/4- CORS/MBA-copy-CORS/utils/fileLogger.js
@@ -0,0 +1,25 @@
+import winston from 'winston';
+const { combine, printf, label, timestamp } = winston.format
+const { File } = winston.transports
+
+/**
+ * Make The String to LOG information
+ * @param {{level: String, message: String, label: String, timestamp: String}} args 
+ */
+const format = printf(({ level, message, label, timestamp }) => {
+    return `[${label}] ["${level}" - ${timestamp}] ${message}`
+})
+
+const fileLogger = winston.createLogger({
+    level: "info",
+    transports: [
+        new(File)({ filename: "logs.log" })
+    ],
+    format: combine(
+        label({ label: "my-bank-api" }),
+        timestamp({ format: "HH:mm:ss" }),
+        format
+    )
+})
+
+export { fileLogger }
