9a2e84b1dae85b26cdda3e3a4c63cd1cc9c31266
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-10-22 05:45:28 -0300

    Update Exercise "Guided Challenge n1" (react-with-hooks)

--- src/3-Frontend/Exercises/Guided-Challenge-n1/react-with-hooks/src/App.js
+++ src/3-Frontend/Exercises/Guided-Challenge-n1/react-with-hooks/src/App.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import React, { useEffect, useState } from 'react'
+import { getDate } from './utils/getDate'
 
-export default class App extends Component {
-  render() {
-    return <ProjetoBase />;
-  }
+export default function App() {
+    const [clickArray, setClickArray] = useState([])
+    useEffect(() => {
+        document.title = clickArray.length.toString()
+    })
+
+    const handleClick = () => {
+        const newClickArray = Object.assign([], clickArray)
+        newClickArray.push(getDate())
+        setClickArray(newClickArray)
+    }
+
+    return (
+        <div>
+            <h2>
+                React usando <em>Hooks</em>
+            </h2>
+            <button onClick={handleClick}>Clique-me!</button>
+
+            <ul>
+                {clickArray.map(item => {
+                    return <li key={item}>{item}</li>
+                })}
+            </ul>
+        </div>
+    )
 }
