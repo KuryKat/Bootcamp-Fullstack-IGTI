a11f27dbb055be29867c80eb1813facaaa0bd80c
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-10-22 05:44:47 -0300

    Update Exercise "Guided-challange-n1" (react-class-components)

--- src/3-Frontend/Exercises/Guided-Challenge-n1/react-class-components/src/App.js
+++ src/3-Frontend/Exercises/Guided-Challenge-n1/react-class-components/src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     }
 
     handleClick = () => {
-        const newClickArray = Object.assign(this.state.clickArray)
+        const newClickArray = Object.assign([], this.state.clickArray)
         newClickArray.push(getDate())
 
         this.setState({ clickArray: newClickArray })
