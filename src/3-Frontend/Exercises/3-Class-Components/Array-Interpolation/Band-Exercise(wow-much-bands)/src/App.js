9cb0d99dad0cc37b2f252be3bb327c7c85131a90
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-10-25 01:30:34 -0300

    Update Exercises Folder (Frontend) [Class-Components]

---
+++ src/3-Frontend/Exercises/3-Class-Components/Array-Interpolation/Band-Exercise(wow-much-bands)/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import Band from './components/Band/Band'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="default-flex-row">
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+                <Band />
+            </div>
+        )
+    }
+}
---
+++ src/3-Frontend/Exercises/3-Class-Components/Array-Interpolation/Band-Exercise(wow-much-bands)/src/components/Band/Band.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import css from './Band.module.css'
+
+export default class Band extends Component {
+    constructor() {
+        super()
+        this.state = {
+            bandName: 'VOCALOID',
+            bandMembers: [
+                {
+                    id: 1,
+                    name: 'Mitchie M',
+                    instrument: 'Hatsune Miku',
+                },
+                {
+                    id: 2,
+                    name: 'GIGA',
+                    instrument: 'Kagamine Rin',
+                },
+                {
+                    id: 3,
+                    name: 'DECO*27',
+                    instrument: 'Megurine Luka',
+                },
+            ],
+        }
+    }
+
+    render() {
+        const { bandName, bandMembers } = this.state
+        const {
+            band,
+            bandName: bandNameStyle,
+            bandMembers: bandMembersStyle,
+        } = css
+        return (
+            <div className={`${band} padding`}>
+                <h3 className={bandNameStyle}>{bandName}</h3>
+                <h5>Members:</h5>
+                <div className={bandMembersStyle}>
+                    {bandMembers.map(({ id, name, instrument }) => {
+                        return (
+                            <ul key={id}>
+                                <li>Name: {name}</li>
+                                <li>Instrument: {instrument}</li>
+                            </ul>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
